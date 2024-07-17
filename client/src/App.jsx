93b9ef1e48057df80bf38ba0b675cab963efa2a0
Kandula Naveen <104118171+Kandulanaveennaidu@users.noreply.github.com>
2024-07-17 09:38:55 +0530

    Create a Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/SingUp";
+import Project from "./pages/Projects"
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline" > EditorialHub </h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/about" element={<About />}/>
+    <Route path="/signin" element={<Signin />}/>
+    <Route path="/signup" element={<Signup />}/>
+    <Route path="/dashboard" element={<Dashboard />}/>
+    <Route path="/projects" element={<Project />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
