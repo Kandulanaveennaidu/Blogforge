d812b295578050f7c5a3798d0d4938b49a3e38fb
Kandula Naveen <104118171+Kandulanaveennaidu@users.noreply.github.com>
2024-07-17 11:44:37 +0530

    I Create Whole Blog-Application

---
+++ client/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPBgzRgcp4WmRyZVyDiI9uKD4uN7REm_0",
+  authDomain: "editorialhub-87ea6.firebaseapp.com",
+  projectId: "editorialhub-87ea6",
+  storageBucket: "editorialhub-87ea6.appspot.com",
+  messagingSenderId: "357400478520",
+  appId: "1:357400478520:web:20bb7dce4190522188b7cd"
+};
+export const app = initializeApp(firebaseConfig);
+
+
